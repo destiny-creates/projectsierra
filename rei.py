b00ed410a917d5f200e584185ebfdb35545419e7
destiny creates <40739698+destiny-creates@users.noreply.github.com>
2026-01-30 13:34:01 -0500

    REI.py V2 Released
    Changelog:
    
    Commented out the "Version" module as it was misbehaving
    
    Added the top_ports port scanning module
    
    Reformatted the file
    
    Changed the output format to better match existing scripts.

--- rei.py
+++ rei.py
@@ -6,7 +6,6 @@ import requests
 #Variables and API keys
 
 nmap = nmap3.Nmap()
-nmapversion = nmap.nmap_version()
 dnsdumpsterapi = 'REDACTED'
 
 print(''' ___  ___  ___ 
@@ -18,15 +17,17 @@ print(''' ___  ___  ___
 print('REI Recon, at your service\n')
 target = input('Target domain:')
 
+
 def processdumpster(data):
     for item in data:
-        print('Host:' + item['host'])
+        print('[+] Host:' + item['host'])
         for ip in item['ips']:
-            print('IP: ' + ip['ip'])
-            print('ASN_NAME:' + ip['asn_name'])
-            print('Country:' + ip['country'])
-            print('City:' + ip.get('city', 'Not found'))
+            print('[+] IP: ' + ip['ip'])
+            print('[+] ASN_NAME:' + ip['asn_name'])
+            print('[+] Country:' + ip['country'])
+            print('[+] City:' + ip.get('city', 'Not found'))
             print('-----------------------')
+
 
 def dumpster():
     url = f'https://api.dnsdumpster.com/domain/{target}'
@@ -44,42 +45,48 @@ def dumpster():
     print('NS records:\n')
     processdumpster(data['ns'])
 
+
 def processdns(target):
     dns_results = nmap.nmap_dns_brute_script(target)
     for item in dns_results:
-        print('Hostname: ' + item['hostname'])
-        print('IP Address: ' + item['address'] + '\n')
+        print('[+] Hostname: ' + item['hostname'])
+        print('[+] IP Address: ' + item['address'] + '\n')
         print('-----------------------')
 
 
-def processversion(target):
-    version_results = nmap.nmap_version_detection(target)
-    ip_address = next(iter(version_results))
-    version = version_results[ip_address]
-    for port in version['ports']:
-        print('Protocol: ' + port['protocol'])
-        print('Port: ' + port['portid'])
-        print('State: ' + port['state'])
-        print('Type: ' + port['reason'])
+# def processversion(target):
+#     version_results = nmap.nmap_version_detection(target)
+#     ip_address = next(iter(version_results))
+#     version = version_results[ip_address]
+#     for port in version['ports']:
+#         print('[+] Protocol: ' + port['protocol'])
+#         print('[+] Port: ' + port['portid'])
+#         print('[+] State: ' + port['state'])
+#         print('-----------------------')
+#         # for service in port['service']:
+#         #     print('Name: ' + port['service'][service])
+
+def processtopports(target):
+    top_ports = nmap.scan_top_ports(target)
+    ports_results = next(iter(top_ports))
+    portid = top_ports[ports_results]
+    for port in portid['ports']:
+        print('[+] Port: ' + port['portid'])
+        print('[+] Protocol: ' + port['protocol'])
+        print('[+] State: ' + port['state'])
         print('-----------------------')
-        # for service in port['service']:
-        #     print('Name: ' + service['name'])
-        #     print('Product: ' + service['product'])
-        #The above commented code is still needing to be worked.
 
-
-def nmapmodule():
-    print('\nNmap version: ' + str(nmapversion))
-    processdns(target)
-    processversion(target)
 
 def main():
     print('Running DNS dumpster module\n')
     dumpster()
     print('Running NMAP DNS module\n')
     processdns(target)
-    print('Running NMAP version detection module\n')
-    processversion(target)
+    print('Running NMAP top ports module\n')
+    processtopports(target)
+    # print('Running NMAP version detection module\n')
+    # processversion(target)
+
 
 if __name__ == '__main__':
     main()
