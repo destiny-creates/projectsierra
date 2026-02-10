1e393334612d2a8306ff8549d65ee45d1e6f334b
destiny creates <40739698+destiny-creates@users.noreply.github.com>
2026-02-09 21:36:09 -0500

    added menu and modules
    Updated the script to now utilize a menu system to match ASH exploitation. This allows you to more precisely control which tests you want to run, saving time.

--- rei.py
+++ rei.py
@@ -1,27 +1,14 @@
 # imports
-
 import subprocess
 
 import nmap3
 import requests
+from pymenu import Menu
 
 # Variables and API keys
 
 nmap = nmap3.Nmap()
-dnsdumpsterapi = 'REDACTED'
-
-subprocess.run('clear', shell=True)
-print('''
-
- ____  _____ ___ 
-|  _ \| ____|_ _|
-| |_) |  _|  | | 
-|  _ <| |___ | | 
-|_| \_\_____|___|
-
-''')
-print('REI Recon, at your service\n')
-target = input('Target domain:')
+dnsdumpsterapi = ''
 
 def processdumpster(data):
     for item in data:
@@ -35,6 +22,7 @@ def processdumpster(data):
 
 
 def dumpster():
+    target = input('[+] Target domain:')
     url = f'https://api.dnsdumpster.com/domain/{target}'
     headers = {'X-Api-Key': dnsdumpsterapi}
     response = requests.get(url, headers=headers)
@@ -44,80 +32,94 @@ def dumpster():
         print('Error connecting to DNSdumpster.')
         exit()
     data = response.json()
-    print('\nA records:\n')
+    print('\n[+] A records:\n')
     processdumpster(data['a'])
-    print('MX records:\n')
+    print('[+] MX records:\n')
     processdumpster(data['mx'])
-    print('NS records:\n')
+    print('[+] NS records:\n')
     processdumpster(data['ns'])
 
 
-def processdns(target):
+def processdns():
+    target = input('[+] Target domain:')
     dns_results = nmap.nmap_dns_brute_script(target)
     for item in dns_results:
-        print('[+] Hostname: ' + item['hostname'])
+        print('\n[+] Hostname: ' + item['hostname'])
         print('[+] IP Address: ' + item['address'] + '\n')
         print('-----------------------')
 
 
-def processversion(target):
+def processversion():
+    target = input('[+] Target domain:')
     version_results = nmap.nmap_version_detection(target)
     ip_address = next(iter(version_results))
     version = version_results[ip_address]
     for port in version['ports']:
-        print('[+] Protocol: ' + port['protocol'])
+        print('\n[+] Protocol: ' + port['protocol'])
         print('[+] Port: ' + port['portid'])
         print('[+] State: ' + port['state'])
         print('[+] Name: ' + port['service']['name'])
         print('-----------------------')
 
 
-def processtopports(target):
+def processtopports():
+    target = input('[+] Target domain:')
     top_ports = nmap.scan_top_ports(target)
     ports_results = next(iter(top_ports))
     port = top_ports[ports_results]
     for port in port['ports']:
-        print('[+] Port: ' + port['portid'])
+        print('\n[+] Port: ' + port['portid'])
         print('[+] Protocol: ' + port['protocol'])
         print('[+] State: ' + port['state'])
         print('[+] Service: ' + port['service']['name'])
         print('-----------------------')
-def processos(target):
+
+
+def processos():
+    target = input('[+] Target domain:')
     os_results = nmap.nmap_os_detection(target)
     ip_address = next(iter(os_results))
     os_version = os_results[ip_address]
     for item in os_version['osmatch']:
-        print('[+] OS: ' + item['name'])
+        print('\n[+] OS: ' + item['name'])
         print('[+] Family: ' + item['osclass']['osfamily'])
         print('[+] Type: ' + item['osclass']['type'])
         print('[+] Generation: ' + item['osclass']['osgen'])
         print('[+] Accuracy: ' + item['accuracy'])
         print('-----------------------')
-def xsstest(target):
+
+
+def xsstest():
+    target = input('[+] Target domain:')
     url = f'https://www.{target}/search?'
     payloads = ['<script>alert(1)</script>', '"><img src=x onerror=alert(1)>']
     for payload in payloads:
         params = {'q': payload}
         r = requests.get(url, params=params)
         if payload in r.text:
-            print(f'[XSS] Payload reflected: {payload}')
+            print(f'\n[XSS] Payload reflected: {payload}')
         else:
-            print(f'[XSS] Not vulnerable to payload: {payload}')
+            print(f'\n[XSS] Not vulnerable to payload: {payload}')
 
-def main():
-    print('\n[+] Running DNS dumpster module\n')
-    dumpster()
-    print('\n[+] Running NMAP DNS module\n')
-    processdns(target)
-    print('\n[+] Running NMAP version detection module\n')
-    processversion(target)
-    print('\n[+] Running NMAP top ports module\n')
-    processtopports(target)
-    print('\n[+] Running NMAP OS detection module\n')
-    processos(target)
-    print('\n[+] Running XSS module\n')
-    xsstest(target)
-    print('\n[+] Happy hunting :)')
 
-if __name__ == '__main__':
-    main()
+def vulnscanmodule():
+    target = input('[+] Target URL/IP Address: ')
+    subprocess.run(f'''nmap --script vuln --script-args mincvss+5.0 {target}''', shell=True)
+
+menu = Menu('''
+
+ ____  _____ ___ 
+|  _ \| ____|_ _|
+| |_) |  _|  | | 
+|  _ <| |___ | | 
+|_| \_\_____|___|
+
+\nREI recon, at your service''')
+menu.add_option('[+] DNS dumpster module', lambda: dumpster())
+menu.add_option('[+] NMAP DNS module', lambda: processdns())
+menu.add_option('[+] NMAP version detection module', lambda: processversion())
+menu.add_option('[+] NMAP top ports module', lambda: processtopports())
+menu.add_option('[+] NMAP os module', lambda: processos())
+menu.add_option('[+] XSS detection module', lambda: xsstest())
+menu.add_option('[+] CVE scanner (NMAP)', lambda: vulnscanmodule())
+menu.show()
